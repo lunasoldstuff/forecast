f483b7499debf153d3088b4014c98feef83e23b6
Robert Wilcox <stampylongr7@gmail.com>
2021-03-10 19:16:38 +0000

    Codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'simplecov'
+require 'codecov'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,4 +12,6 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  SimpleCov.start
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
