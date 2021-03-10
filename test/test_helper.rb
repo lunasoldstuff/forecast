84110731c617f70327682c0f27094e3cc88f3995
luna <yo@themintfarm.ml>
2021-03-10 15:51:04 -0500

    fix codecov
    Oops
    
    Co-authored-by: Robert Wilcox <yo@themintfarm.ml>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'simplecov'
+SimpleCov.start
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
